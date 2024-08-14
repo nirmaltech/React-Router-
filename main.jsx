21e91f1e2f04783491618596decb4b94df4653f8
Nirmal Kumar <161622261+nirmaltech@users.noreply.github.com>
2024-08-14 20:53:02 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,50 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+//import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import Home from './components/Home/Home.jsx'
+import './index.css'
+
+/*//const router = createBrowserRouter([
+
+ // {
+   // path: '/',
+    //element: <Layout/>,
+    //children: [
+      {
+        path: '',
+        element: <Home/>
+      },
+      {
+        path: 'about',
+        element: <About/>
+      },
+      {
+        path: "contact",
+        element: <Contact/>
+      }
+   // ]
+  //}
+//])*/
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+
+
+    </Route>
+  )
+
+)
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
